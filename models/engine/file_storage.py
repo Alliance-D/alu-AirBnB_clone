84d5be8f0c180fe22b8bf8553462e206e626d50d
Alliance-D <a.dushimezi@alustudent.com>
2024-09-14 08:10:04 -0700

    file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This module defines a FileStorage class that stores
+and retrieves objects to and from a JSON file.
+"""
+
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+class FileStorage:
+    """Serializes instances to a JSON file and deserializes JSON file to instances."""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary of objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file."""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects, if it exists."""
+        try:
+            with open(self.__file_path, 'r') as f:
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                class_name = value['__class__']
+                if class_name in globals():
+                    self.__objects[key] = globals()[class_name](**value)
+        except FileNotFoundError:
+            pass
